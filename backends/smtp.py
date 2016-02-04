460a8687104795da829d24d6ae4fbbc22811264b
Anders Fugmann <anders@fugmann.net>
2016-02-05 00:43:15 +0100

    All works if its installed

--- backends/smtp.py
+++ backends/smtp.py
@@ -10,17 +10,17 @@ import subprocess
 
 def pdf2html(data):
     base = "eboks_" + str(os.getpid())
-    ifile = filename + ".pdf"
-    ofile = filename + ".html"
-    infp = file("/tmp/" + ifile, 'rw')
-    infp.write(data)
-    infp.close()
-    subprocess.call(["/usr/bin/pdf2htmlEX", ifile, ofile], cwd="/tmp")
-    outfp = file("/tmp/" + ofile, 'r')
-    res = outfp.read()
-    outfp.close()
-    os.remove("/tmp/" + ifile)
-    os.remove("/tmp/" + gfile)
+    pdffile = base + ".pdf"
+    htmlfile = base + ".html"
+    fp = file("/tmp/" + pdffile, 'w')
+    fp.write(data)
+    fp.close()
+    subprocess.call(["/usr/bin/pdf2htmlEX", pdffile, htmlfile], cwd="/tmp")
+    fp = file("/tmp/" + htmlfile, 'r')
+    res = fp.read()
+    fp.close()
+    os.remove("/tmp/" + pdffile)
+    os.remove("/tmp/" + htmlfile)
     return res
 
 class Backend(object):
@@ -47,7 +47,7 @@ class DocumentCollection(object):
 
     def attach(self, filename, mimetype, binary):
         html = pdf2html(binary)
-        self.msg.attach(MIMEText(html, 'html'))
+        self.msg.attach(MIMEText(html, 'html', _charset='utf-8'))
 
         part = MIMEBase(*mimetype.split("/"))
         part.set_payload(binary)

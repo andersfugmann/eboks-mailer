73cb34c10ce8a062e7595079f76a9f018b1d07e6
Anders Fugmann <anders@fugmann.net>
2016-02-04 22:23:02 +0100

    Add html preview

--- backends/smtp.py
+++ backends/smtp.py
@@ -2,8 +2,33 @@ import smtplib
 
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
+from email.mime.text import MIMEText
 from email import Encoders
 
+import StringIO
+import pdfminer.pdfinterp
+import pdfminer.pdfpage
+import pdfminer.converter
+
+# Pdf conversion to HTML so the mails have a body
+def pdf2html(data):
+    outfp = StringIO.StringIO()
+    infp = StringIO.StringIO(data)
+    rsrcmgr = pdfminer.pdfinterp.PDFResourceManager(caching=True)
+    device = pdfminer.converter.HTMLConverter(rsrcmgr, outfp, codec='utf-8', scale=1,
+                           layoutmode='normal',
+                           laparams=pdfminer.layout.LAParams(),
+                           imagewriter=None)
+    interpreter = pdfminer.pdfinterp.PDFPageInterpreter(rsrcmgr, device)
+    for page in pdfminer.pdfpage.PDFPage.get_pages(infp, set(),
+                                                   maxpages=0, password=None,
+                                                   caching=True, check_extractable=True):
+        interpreter.process_page(page)
+
+    device.close()
+    res = outfp.getvalue()
+    outfp.close()
+    return res
 
 class Backend(object):
 
@@ -22,12 +47,15 @@ class DocumentCollection(object):
         self.backend = backend
         self.msg = MIMEMultipart()
         self.msg['To'] = backend.mailto
- 
+
     def set_metadata(self, subject, sender, date):
-        self.msg['Subject'] = subject 
+        self.msg['Subject'] = subject
         self.msg['From'] = self.backend.mailfrom % sender
 
-    def attach(self, filename, mimetype, binary):   
+    def attach(self, filename, mimetype, binary):
+        html = pdf2html(binary)
+        self.msg.attach(MIMEText(html, 'html'))
+
         part = MIMEBase(*mimetype.split("/"))
         part.set_payload(binary)
         Encoders.encode_base64(part)
@@ -37,4 +65,3 @@ class DocumentCollection(object):
     def execute(self):
         server = smtplib.SMTP(self.backend.mailserver)
         server.sendmail(self.msg['From'], self.backend.mailto, self.msg.as_string())
-        
